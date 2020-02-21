eb8a4f7be171d9b673f90c92ee2cbdbba50ff607
soraya-s <sosh9992@fiji-1.colorado.edu>
2020-02-21 12:27:00 -0700

    paired end for prudencio fcx

--- run.sh
+++ run.sh
@@ -15,7 +15,7 @@ module load singularity/3.1.1
 nextflow run nf-core/rnaseq -r 1.4.1 \
 -resume \
 -profile singularity \
---singleEnd --reads './data/*.fastq' \
+--reads './data/prudencio_fcx/*_{1,2}.fastq' \
 --fasta './GENOMES/sequence/GRCh38.p13.genome.fa' \
 --gtf './GENOMES/annotation/gencode.v32.annotation.gtf' \
 --pseudo_aligner salmon \
